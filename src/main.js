d79f7212def3006df8fb517ce24d085f6743d2cb
Anezijane <ahnnieanezi@gmail.com>
2024-01-22 00:06:24 +0100

    add month/year/day calculated age

--- src/main.js
+++ src/main.js
@@ -20,12 +20,27 @@ showDate.addEventListener('click', (e)=> {
     let monthValue =parseInt(month.value,10);
     let yearValue = parseInt(year.value,10);
 
-     console.log(dayValue);
+     //console.log(dayValue);
+
+    // to Get current date
+    var currentDate = new Date();
+
+    // to Create a date object with the entered date
+    var enteredDate = new Date(yearValue, monthValue - 1, dayValue);
+
+    // Calculate the difference in milliseconds
+    var dateDifference = currentDate - enteredDate;
+
+    // Convert milliseconds to years, months, and days
+    var years = Math.floor(dateDifference / (365.25 * 24 * 60 * 60 * 1000));
+    var months = Math.floor((dateDifference % (365.25 * 24 * 60 * 60 * 1000)) / (30.44 * 24 * 60 * 60 * 1000));
+    var days = Math.floor((dateDifference % (30.44 * 24 * 60 * 60 * 1000)) / (24 * 60 * 60 * 1000));
+
 
      // Check if day, month, and year are valid
     {
         if (dayValue >= 1 && dayValue <= 31) {
-            displayDay.innerHTML = dayValue;
+            displayDay.innerHTML = days.valueOf(days);
             errorDay.innerHTML = '';
             clearError(day, dayLabel);
         } else {
@@ -35,7 +50,7 @@ showDate.addEventListener('click', (e)=> {
     }
     {
         if (monthValue >= 1 && monthValue <= 12) {
-            displayMonth.innerHTML = monthValue;
+            displayMonth.innerHTML = months.valueOf(days);
             errorMonth.innerHTML = '';
             clearError(month, monthLabel);
         } else {
@@ -45,7 +60,7 @@ showDate.addEventListener('click', (e)=> {
     }
     {
         if (yearValue >= 1900 && yearValue <= 2024) {
-            displayYear.innerHTML = yearValue;
+            displayYear.innerHTML = years.valueOf(years);
             errorYear.innerHTML = '';
             clearError(year, yearLabel);
         } else {
@@ -53,9 +68,8 @@ showDate.addEventListener('click', (e)=> {
            displayError(year, yearLabel);
         }
     }
-    
  
-    
+
      day.value = ''; // to clear the value in the input box
      month.value = ''; 
      year.value = ''; 
@@ -70,6 +84,18 @@ function clearError(value, label) {
     label.classList.remove('text-red-500');
     value.classList.remove('border-red-500')
 }
+function requiredField() {
+    errorDay.innerHTML = "This field is required";
+    displayError(day, dayLabel);
+}
 
-
-
+// function inputContent() {
+//     // Check if any of the input values are empty
+//     if (isNaN(day.Value)) {
+//         requiredField();
+//       } else if (isNaN(month.Value)) {
+//         requiredField();
+//       } else if (isNaN(year.Value)) {
+//         requiredField();
+//       }
+// }
