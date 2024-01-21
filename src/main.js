8af97dfcfd951abeee311bc6a0151be06c552921
Anezijane <ahnnieanezi@gmail.com>
2024-01-21 21:40:26 +0100

    add day validation and display

--- src/main.js
+++ src/main.js
@@ -2,12 +2,44 @@ let day = document.getElementById('day')
 let month = document.getElementById('month')
 let year = document.getElementById('year')
 
-let displayYear = document.getElementById('displayYear');
-let displayMonth = document.getElementById('displayMonth');
-let displayDay = document.getElementById('displayDay');
+let displayYear = document.getElementById('displayYear')
+let displayMonth = document.getElementById('displayMonth')
+let displayDay = document.getElementById('displayDay')
 
-let errorDay = document.getElementById('day-result');
-let errorMonth = document.getElementById('month-result');
-let errorYear = document.getElementById('year-result');
+let errorDay = document.getElementById('day-result')
+let errorMonth = document.getElementById('month-result')
+let errorYear = document.getElementById('year-result')
+
+let showDate = document.getElementById('img')
+showDate.addEventListener('click', (e)=> {
+    e.preventDefault(); // to prevent default form submission
+    let dayValue = parseInt(day.value,10); // to get the value in the input
+    // let monthValue =parseInt(month.value,10);
+    // let yearValue = parseInt(year.value,10);
+
+     console.log(dayValue);
+
+     // Check if day, month, and year are valid
+    {
+        if (dayValue >= 1 && dayValue <= 31) {
+            displayDay.innerHTML = dayValue;
+            errorDay.innerHTML = '';
+        } else {
+            errorDay.innerHTML = "must be a valid day";
+        }
+    }
+     
+ 
+    
+     day.value = ''; // to clear the value in the input box
+    //  month.value = ''; 
+    //  year.value = ''; 
+
+})
+
+// function updateResult(content) {
+//     displayDay.textContent = content;
+// }
+
 
 
